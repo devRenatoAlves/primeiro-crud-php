e759e8a8c538e9c38bd62d47a97499841737c657
Renato Alves <devrenatoalves@gmail.com>
2025-11-16 22:45:08 -0300

    Página inicial criada com controller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MangaController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [MangaController :: class, 'mangaPage']);
