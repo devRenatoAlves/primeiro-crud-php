c312acac3835bcdf38d9bc7605dd2a6b073fcec5
Renato Alves de Matos <devrenatoalves@gmail.com>
2025-11-24 13:33:16 -0300

    Criando formulário padrão

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,6 +16,32 @@
           <div>
       </nav>
 
+      <form>
+
+      <div class="form-group col-md-4">
+        <label for="formGroupExampleInput">Nome do Mangá:</label>
+        <input type="text" class="form-control" id="formGroupExampleInput" placeholder="Dragon Ball">
+      </div>  
+
+      <div class="form-group col-md-4">
+        <label for="formGroupExampleInput2">Quantidade de Páginas:</label>
+        <input type="number" class="form-control" id="formGroupExampleInput2" placeholder="145">
+      </div>
+
+      <div class="form-group col-md-4">
+      <label for="inputState">Status</label>
+        <select id="inputState" class="form-control">
+          <option selected>Escolha...</option>
+          <option>Lendo</option>
+          <option>Dropei</option>
+          <option>Finalizado</option>
+          <option>Planejo Ler</option>
+        </select>
+      </div>
+
+    </form>
+
+
     <script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
