8d69184a7b6f50761d0d9b3192d3326071fba52e
Renato Alves de Matos <devrenatoalves@gmail.com>
2025-11-30 23:53:54 -0300

    Preparando query de create com validação

--- app/Http/Controllers/MangaController.php
+++ app/Http/Controllers/MangaController.php
@@ -1,24 +1,36 @@
 <?php
 
 namespace App\Http\Controllers;
-
 use Illuminate\Http\Request;
+use App\Models\Manga;
 
 class MangaController extends Controller
 {   
-    public function mangaStore () {
+    public function mangaStore (Request $request) { //cria as info no db
+        $request -> validate([
+            'manga_name' => 'require|max:255',
+            'qtd_pg' => 'require|integer|min:1',
+            'status' => 'require',
+        ]);
+
+        Manga::create([
+            'manga_name' => $request->manga_name,
+            'qtd_pg' => $request->qtd_page,
+            'status' => $request->status,
+        ]);
+
+        return redirect()->route('manga.index')->with('success', 'Mangá inserido com sucesso!');
+    }
+
+    public function mangaIndex () { // le as info no db
         return ;
     }
 
-    public function mangaIndex () {
+    public function mangaUpdate () { // atualiza as info no db
         return ;
     }
 
-    public function mangaUpdate () {
-        return ;
-    }
-
-    public function mangaDestroy () {
+    public function mangaDestroy () { // exclui as info no db
         return ;
     }
 
