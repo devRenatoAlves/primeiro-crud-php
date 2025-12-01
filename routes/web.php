fcf251d0db910b936c565f5da87a5ad2f5726448
Renato Alves de Matos <devrenatoalves@gmail.com>
2025-11-30 23:53:24 -0300

    Rascunho das rotas de querys

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MangaController;
 
 Route::get('/', [MangaController :: class, 'mangaPage']);
+
+Route::get('/mangaindex', [MangaController :: class, 'mangaIndex'])->name('manga.index');
+
+Route::post('/mangastore', [MangaController :: class, 'mangaStore'])->name('manga.store');
